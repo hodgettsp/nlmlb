5bae95ead9c2533880d5616fc840387c17fc572d
Paul A. Hodgetts <hodgetts.paul@outlook.com>
2021-04-16 23:34:55 -0400

    finished data prep

--- script/prep/wikipedia_black_player_prep.R
+++ script/prep/wikipedia_black_player_prep.R
@@ -22,72 +22,134 @@ td_data <- raw_black_data_mlb %>%
      # removes extraneous html text and code
      html_text()
 
-t1 <- tibble(player_data = td_data)
+# create temporary tibble from td tagged html data
+t1 <- tibble(player_data = td_data) %>%
+        # this adds data that is not included due to the html structure
+        # add date before row 73
+        add_row(player_data = "April 18, 1951", .before = 73) %>%
+        # add date before row 77
+        add_row(player_data = "April 18, 1951", .before = 77) %>%
+        # add team before row 114
+        add_row(player_data = "New York Giants", .before = 114) %>%
+        # add league before row 115
+        add_row(player_data = "NL", .before = 115) %>%
+        # add date before row 116
+        add_row(player_data = "July 8, 1949‡", .before = 116) %>%
+        # add team before row 146
+        add_row(player_data = "Cincinnati Reds", .before = 146) %>%
+        # add league before row 147
+        add_row(player_data = "NL", .before = 147) %>%
+        # add date before row 148
+        add_row(player_data = "April 17, 1954", .before = 148)
 
-t1 <- add_row(t1, player_data = "April 18, 1951", .before = 73)
+# assign new data object to hold ->
+# pre-integration data values
+pre_integration <- t1 %>%
+        slice(1:15)
 
-t1 <- add_row(t1, player_data = "April 18, 1951", .before = 77)
+# create new function to run repetitive data prep
+# take four parameters of a variable name, dataset, rows to slice, ->
+# column names to be used, and key column to count
+prep_data <- function(.var, .data, .slice, ..., .no){
+        # assign new variable and slice provided dataset
+        assign(.var, slice(.data, .slice) %>%
+                       # mutate new column key to hold new column names as values ->
+                       # and reset after every 4th row
+                       mutate(key = rep(c(...), n()/4),
+                              # create new column to hold tracker number
+                              no = cumsum(key == .no)) %>%
+                       # pivot data from long to wide with column names from key column ->
+                       # and values from player_data
+                       pivot_wider(names_from = key, values_from = .data$player_data) %>%
+                       # remove no column
+                       select(-no),
+               # assign to global environment
+               envir = .GlobalEnv)
+}
 
-t1 <- add_row(t1, player_data = "New York Giants", .before = 114)
-t1 <- add_row(t1, player_data = "NL", .before = 115)
-t1 <- add_row(t1, player_data = "July 8, 1949‡", .before = 116)
+# run prep_data function to create new dataset first20
+prep_data(.var = "first20", .data = t1, .slice = 18:97,
+                 "player", "mlb_team", "league", "date", .no = "player")
 
-t1 <- add_row(t1, player_data = "Cincinnati Reds", .before = 146)
-t1 <- add_row(t1, player_data = "NL", .before = 147)
-t1 <- add_row(t1, player_data = "April 17, 1954", .before = 148)
+# run prep data function to create new dataset first_for_team
+prep_data(.var = "first_for_team", .data = t1, .slice = 98:169,
+          "mlb_team", "league", "date", "player", .no = "mlb_team")
 
-
-t2 <- t1 %>%
-     slice(1:15)
-
-t3 <- t1 %>%
-     slice(18:97) %>%
-     mutate(key = rep(c("player", "mlb_team", "league", "date"), n()/4),
-            no = cumsum(key == "player")) %>%
-     pivot_wider(names_from = key, values_from = player_data) %>%
-     select(-no) %>%
+# clean first20 dataframe
+first20 <- first20 %>%
      mutate(player = str_replace(player, "\\s\\W*$", ""),
-            player = str_replace(player, "^\\n", ""),
+            # strip newline character at beginning of names
+            player = str_replace(player, "(^\\r\\n)", ""),
+            # create new column year from date column ->
+            # using last word characters
             year = str_extract(date, "\\w*$"),
+            # set year values as numeric
             year = as.numeric(year),
+            # extract first name from player column ->
+            # as starting word characters until first non-word chracter
             firstname = str_extract(player, "^\\w*"),
-            lastname = str_extract(player, "\\w*$"))
+            # extract last name from player column ->
+            # as final word characters
+            lastname = str_extract(player, "\\w*$"),
+            # convert date to date type class
+            date = lubridate::parse_date_time(date, orders = c("bdy", "bY")))
 
-t4 <- t1 %>%
-     slice(98:169) %>%
-     mutate(key = rep(c("mlb_team", "league", "date", "player"), n()/4),
-            no = cumsum(key == "mlb_team")) %>%
-     pivot_wider(names_from = key, values_from = player_data) %>%
-     select(-no) %>%
+# clean data object for first Black players on a team
+first_for_team <- first_for_team %>%
+     # strip non-word characters from player names
      mutate(player = str_replace(player, "(\\s\\W*$|\\W*$|\\[.*])", ""),
-            player = str_replace(player, "^\\n*", ""),
+            # strip newline from player names
+            player = str_replace(player, "(^\\r*\\n*)", ""),
+            # create first name from player column ->
+            # as first word characters until nonword character
             firstname = str_extract(player, "^\\w*"),
+            # create last name from player using string_match for second match group ->
+            # in which last name is all characters after the first space
             lastname = str_match(player, "(?:[^\\s]*\\s){1}([^\\s]*\\w.*$)")[,2],
+            # strip special characters from date column
             date = str_replace(date, "\\W*$", ""),
+            # create year column from date as last word characters
             year = str_extract(date, "\\w*$"),
-            year = as.numeric(year)) %>%
+            # set year as numeric
+            year = as.numeric(year),
+            # convert date to date type class
+            date = lubridate::parse_date_time(date, orders = c("bdy", "bY"))) %>%
+     # use select to reorder columns to match first20 dataframe
      select(player, mlb_team, league, date, year, firstname, lastname)
 
-t4 <- t4 %>%
-     filter(year >= 1953)
+# create temporary place holder for years 1953 and above
+t4 <- first_for_team %>%
+        filter(year >= 1953)
 
-t5 <- bind_rows(t3, t4)
 
 # this data from http://research.sabr.org/journals/integration-of-baseball-after-world-war-ii
+# create new tibble to hold data for players who debuted in 1952
 season52 <- tibble(player = c("Héctor Rodríguez", "George Crowe", "Buster Clarkson",
                               "Quincy Trouppe", "Dave Pope", "Sandy Amorós"),
                    mlb_team = c("Chicago White Sox", "Boston Braves", "Boston Braves",
                                 "Cleveland Indians", "Cleveland Indians", "Brooklyn Dodgers"),
                    league = c("AL", "NL", "NL", "AL", "AL", "NL"),
-                   date = c("April 15, 1952", "April 17, 1952",
-                            "April 30, 1952", "April 30 1952",
-                            "July 1, 1952", "August 22, 1952"),
+                   date = c("1952-04-15", "1952-04-17",
+                            "1952-04-30", "1952-04-30",
+                            "1952-07-01", "1952-08-22"),
                    year = 1952,
                    firstname = c("Héctor", "George", "Buster", "Quincy", "Dave", "Sandy"),
-                   lastname = c("Rodríguez", "Crowe", "Clarkson", "Trouppe", "Pope", "Amorós"))
+                   lastname = c("Rodríguez", "Crowe", "Clarkson", "Trouppe", "Pope", "Amorós")) %>%
+        mutate(date = as.Date(date, format = "%Y-%m-%d"))
 
 
-t6 <- bind_rows(t5, season52) %>%
-     arrange(year) %>%
-     mutate(suffix = str_extract(lastname, "Sr."),
-            lastname = str_replace(lastname, "\\s\\w*\\W*$", ""))
+# bind created datasets together
+black_players_mlb <- bind_rows(first20, t4, season52) %>%
+        # arrange by year
+        arrange(year) %>%
+        # create new column to hold Sr. suffix
+        mutate(suffix = str_extract(lastname, "Sr."),
+               # strip last name of extra characters
+               lastname = str_replace(lastname, "\\s\\w*\\W*$", ""))
+
+
+# write data to csv
+write_csv(black_players_mlb, "inputs/data/csv/black_player_mlb_data.csv")
+
+# clear environment
+rm(list = ls())
