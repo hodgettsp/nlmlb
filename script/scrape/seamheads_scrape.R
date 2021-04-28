516c2e0206170d1393bf6d73552e38a97a5c58a6
Paul A. Hodgetts <hodgetts.paul@outlook.com>
2021-04-28 00:28:46 -0400

    data is maybe fixed?

--- script/scrape/seamheads_scrape.R
+++ script/scrape/seamheads_scrape.R
@@ -3,28 +3,28 @@ library(rvest)
 library(tidyverse)
 
 
-nl_data_8620 <- read_html("https://www.seamheads.com/NegroLgs/history.php?tab=players&first=1886&last=1920&lgID=All&lgType=All&NeL=All&HOF=All&pos=All&bats=All&throws=All&results=2500&sort=Player_a")
-xml2::write_html(nl_data_8620, "inputs/data/seamheads/raw_data_8620.html")
+nl_data_8636 <- read_html("https://www.seamheads.com/NegroLgs/history.php?tab=players&first=1886&last=1936&lgID=All&lgType=N&NeL=Y&HOF=All&pos=All&bats=All&throws=All&results=2500&sort=Player_a")
+xml2::write_html(nl_data_8636, "inputs/data/seamheads/raw_data_8636.html")
 
-nl_data_2035 <- read_html("https://www.seamheads.com/NegroLgs/history.php?tab=players&first=1920&last=1935&lgID=All&lgType=All&NeL=All&HOF=All&pos=All&bats=All&throws=All&results=2500&sort=Player_a")
-xml2::write_html(nl_data_2035, "inputs/data/seamheads/raw_data_2035.html")
+nl_data_3748 <- read_html("https://www.seamheads.com/NegroLgs/history.php?tab=players&first=1937&last=1948&lgType=All&NeL=Y&HOF=All&pos=All&bats=All&throws=All&results=2500&sort=Player_a")
+xml2::write_html(nl_data_3748, "inputs/data/seamheads/raw_data_3748.html")
 
-nl_data_3048 <- read_html("https://www.seamheads.com/NegroLgs/history.php?tab=players&first=1930&last=1948&lgID=All&lgType=All&NeL=All&HOF=All&pos=All&bats=All&throws=All&results=2500&sort=Player_a")
-xml2::write_html(nl_data_3048, "inputs/data/seamheads/raw_data_3048.html")
+nl_data_3648 <- read_html("https://www.seamheads.com/NegroLgs/history.php?tab=players&first=1936&last=1948&lgID=All&lgType=All&NeL=All&HOF=All&pos=All&bats=All&throws=All&results=2500&sort=Player_a")
+xml2::write_html(nl_data_3648, "inputs/data/seamheads/raw_data_3648.html")
 
-nl_data_8620 <- read_html("inputs/data/seamheads/raw_data_8620.html")
-nl_data_2035 <- read_html("inputs/data/seamheads/raw_data_2035.html")
-nl_data_3048 <- read_html("inputs/data/seamheads/raw_data_3048.html")
+nl_data_8636 <- read_html("inputs/data/seamheads/raw_data_8636.html")
+nl_data_3748 <- read_html("inputs/data/seamheads/raw_data_3748.html")
+nl_data_3648 <- read_html("inputs/data/seamheads/raw_data_3648.html")
 
-text_data <- nl_data_8620 %>%
+text_data <- nl_data_8636 %>%
      html_nodes("td") %>%
      html_text()
 
 table_data <- tibble(player_data = text_data)
 
-player_data_8620 <- slice(table_data, 13:25544)
+player_data_8636 <- slice(table_data, 13:29678)
 
-player_data_8620 <- player_data_8620 %>%
+player_data_8636 <- player_data_8636 %>%
         mutate(player_data = str_replace(player_data, "^\\s*", ""),
                player_data = str_replace(player_data, "\\s*$", ""),
                key = rep(c("id", "player", "years", "pos", "pa", "ip",
@@ -39,15 +39,15 @@ player_data_8620 <- player_data_8620 %>%
 
 
 
-text_data2035 <- nl_data_2035 %>%
+text_data3748 <- nl_data_3748 %>%
         html_nodes("td") %>%
         html_text()
 
-table_data2035 <- tibble(player_data = text_data2035)
+table_data3748 <- tibble(player_data = text_data3748)
 
-player_data_2035 <- slice(table_data2035, 13:24803)
+player_data_3748 <- slice(table_data3748, 13:18277)
 
-player_data_2035 <- player_data_2035 %>%
+player_data_3748 <- player_data_3748 %>%
         mutate(player_data = str_replace(player_data, "^\\s*", ""),
                player_data = str_replace(player_data, "\\s*$", ""),
                key = rep(c("id", "player", "years", "pos", "pa", "ip",
@@ -60,63 +60,114 @@ player_data_2035 <- player_data_2035 %>%
 
 
 
-
-
-text_data3048 <- nl_data_3048 %>%
-        html_nodes("td") %>%
-        html_text()
-
-table_data3048 <- tibble(player_data = text_data3048)
-
-player_data_3048 <- slice(table_data3048, 13:30341)
-
-player_data_3048 <- player_data_3048 %>%
-        mutate(player_data = str_replace(player_data, "^\\s*", ""),
-               player_data = str_replace(player_data, "\\s*$", ""),
-               key = rep(c("id", "player", "years", "pos", "pa", "ip",
-                           "drop", "ht", "wt", "b", "t", "birth", "death"),
-                         n()/13),
-               no = cumsum(key == "id"))%>%
-        pivot_wider(names_from = key, values_from = player_data) %>%
-        select(-no, -drop)
-
-x <- player_data_2035 %>%
+x <- player_data_8636 %>%
         mutate(year1 = str_match(years, "^\\d*"),
                year2 = str_match(years, "\\d*$"),
                year1 = as.numeric(year1),
                year2 = as.numeric(year2),
-               birth = if_else(((year2 < 1935) & (year1 > 1920)) & (birth == ""), "no", birth))
+               birth = if_else((year2 < 1936) & (birth == ""), "no", birth))
 
-y <- player_data_8620 %>%
+y <- player_data_3748 %>%
         mutate(year1 = str_match(years, "^\\d*"),
                year2 = str_match(years, "\\d*$"),
                year1 = as.numeric(year1),
-               year2 = as.numeric(year2),
-               birth = if_else((year2 < 1920) & (birth == ""), "ok", birth))
+               year2 = as.numeric(year2))
+               #birth = if_else(((year1 > 1921) & (year2 < 1935)) & (birth == ""), "ok", birth))
 
-z <- player_data_3048 %>%
+z <- x %>%
+        full_join(y, by = c("player", "ht", "wt", "b", "t", "birth", "death")) %>%
+        select(player, years.x, years.y, year1.x, year2.x, year1.y, year2.y) %>%
+        rename(year1 = year1.x,
+               year2 = year2.x,
+               year3 = year1.y,
+               year4 = year2.y) %>%
+        mutate(debut = case_when(!is.na(year1) ~ year1,
+                                 (is.na(year1) & !is.na(year3)) ~ year3),
+               final = case_when(!is.na(year4) ~ year4,
+                                 (is.na(year4) & !is.na(year2)) ~ year2)) %>%
+        select(-year1, -year2, -year3, -year4)
+
+
+a <- z %>%
+        count(debut) %>%
+        rename(debut_count = n,
+               season = debut)
+
+b <- z %>%
+        count(final) %>%
+        rename(final_count = n,
+               season = final)
+
+c <- inner_join(a, b, by = "season") %>%
+        mutate(player_count = 0)
+
+
+# for index i in the length of player_count starting at second value
+for(i in 2:length(c$player_count)){
+        # make player_count at index location equal to->
+        # debut count of that year + player count of previous year - final count of previous year
+        # this gives an accurate count of the number of Negro League players for a season by ->
+        # adding the number of new players to the number of previous players minus the number of players ->
+        # no longer playing
+        c$player_count[i] <- (c$debut_count[i] +
+                                      c$player_count[i-1]) -
+                c$final_count[i-1]
+}
+
+
+
+
+
+
+
+z <- player_data_3648 %>%
         mutate(year1 = str_match(years, "^\\d*"),
                year2 = str_match(years, "\\d*$"),
                year1 = as.numeric(year1),
-               year2 = as.numeric(year2),
-               birth = if_else((year1 < 1948) & (birth == ""), "go", birth))
+               year2 = as.numeric(year2))
+               #birth = if_else(((year1 < 1948) & (year2 )) & (birth == ""), "go", birth))
+a <- x %>%
+        full_join(y, by = c("player", "ht", "wt", "b", "t", "birth", "death")) %>%
+        full_join(z, by = c("player", "ht", "wt", "b", "t", "birth", "death")) %>%
+        select(player, years.x, years.y, years)
 
-x <- player_data_8620 %>%
-        full_join(player_data_2035, by = c("player", "ht", "wt", "b", "t", "birth", "death")) %>%
-        full_join(player_data_3048, by = c("player", "ht", "wt", "b", "t", "birth", "death"))
 
-
-y <- x %>%
+a <- a %>%
         mutate(year1 = str_match(years.x, "^\\d*"),
                year2 = str_match(years.x, "\\d*$"),
                year3 = str_match(years.y, "^\\d*"),
                year4 = str_match(years.y, "\\d*$"),
                year5 = str_match(years, "^\\d*"),
-               year6 = str_match(years, "\\d*$"))
+               year6 = str_match(years, "\\d*$"),
+               debut = case_when(!is.na(year1) ~ year1,
+                                 (is.na(year1) & !is.na(year3)) ~ year3,
+                                 (is.na(year1) & is.na(year3) & !is.na(year5)) ~ year5),
+               final = case_when(!is.na(year6) ~ year6,
+                                 (is.na(year6) & !is.na(year4)) ~ year4,
+                                 (is.na(year6) & is.na(year4) & !is.na(year2)) ~ year2))
 
-x <- x %>%
-        mutate(years.x = if_else((!is.na(years.x) & !is.na(years.y)) | (is.na(years.x)), years.y, years.x)) %>%
-        mutate(years.x = if_else((!is.na(years.x) & !is.na(years)) | (is.na(years.x)), years, years.x))
+b <- a %>%
+        count(debut) %>%
+        rename(debut_count = n,
+               season = debut)
+
+c <- a %>%
+        count(final) %>%
+        rename(final_count = n,
+               season = final)
+
+d <- inner_join(b, c, by = "season") %>%
+        mutate(player_count = 0)
 
 
-
+# for index i in the length of player_count starting at second value
+for(i in 2:length(d$player_count)){
+        # make player_count at index location equal to->
+        # debut count of that year + player count of previous year - final count of previous year
+        # this gives an accurate count of the number of Negro League players for a season by ->
+        # adding the number of new players to the number of previous players minus the number of players ->
+        # no longer playing
+        d$player_count[i] <- (d$debut_count[i] +
+                                      d$player_count[i-1]) -
+                d$final_count[i-1]
+}
